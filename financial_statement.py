d97f73c0df0c6d28cc6a0f1136f4cf2518806acf
Max <haiching.tsai@gmail.com>
2012-06-29 06:13:10 +0800

    update finance statement fetch. remove quote data.

--- financial_statement.py
+++ financial_statement.py
@@ -2,36 +2,51 @@
 
 #import urllib.request
 import urllib
-from HTMLParser import HTMLParser
+import lxml.html as L
+import lxml.html.soupparser as S
 
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-	return ''.join(self.fed)
+class MOPS_fetch:
+	def __init__(self):
+		self.balance_sheet_url = "http://mops.twse.com.tw/mops/web/t05st31"
+		self.income_statement_url = "http://mops.twse.com.tw/mops/web/t05st32"
+		self.stock_holder_change_url = "http://mops.twse.com.tw/mops/web/t05st35"
+		self.cash_change_url = "http://mops.twse.com.tw/mops/web/t05st36"
+		self.parms = {}
+		self.parms['encodeURIComponent'] = 1
+		self.parms['step'] = 1
+		self.parms['firstin'] = 1
+		self.parms['off'] = 1
+		self.parms['keybord4'] = ''
+		self.parms['code1'] = ''
+		self.parms['TYPEK2'] = ''
+		self.parms['checkbtn'] = ''
+		self.parms['queryName'] = 'co_id'
+		self.parms['TYPEK'] = 'all'
+		self.parms['isnew'] = 'true'
+		self.parms['co_id'] = '2454'
+		self.parms['year'] = ''
+		self.parms['season'] = ''
+
+		self.accounts = []
+	def _test(self):
+		parm = urllib.urlencode(self.parms)
+		fd = urllib.urlopen("http://mops.twse.com.tw/mops/web/t05st31", parm)
+		page = fd.read()
+		fd.close()
+
+		dom = L.fromstring(page)
+
+		'''
+		Firebug & Firepath are great tools. Firebug can show request content, 
+		and Firepath can locate tag.
+		ps: Firepath adds 'tbody' tag, remove it.
+		'''
+		r = dom.xpath(".//*[@id='table01']/center/table/tr[8]/td[2]")
+		r1 = dom.xpath(".//*[@id='table01']/center/table/tr[4]/th[2]/b")
+		print r[0].text
+		print r1[0].text
 
 
-parms = {}
-parms['encodeURIComponent'] = 1
-parms['step'] = 1
-parms['firstin'] = 1
-parms['off'] = 1
-parms['keybord4'] = ''
-parms['code1'] = ''
-parms['TYPEK2'] = ''
-parms['checkbtn'] = ''
-parms['queryName'] = 'co_id'
-parms['TYPEK'] = 'all'
-parms['isnew'] = 'true'
-parms['co_id'] = '2454'
-parms['year'] = ''
-parms['season'] = ''
-parm = urllib.urlencode(parms)
-#print parm
-text = urllib.urlopen("http://mops.twse.com.tw/mops/web/t05st20", parm).read()
-s = MLStripper()
-s.feed(text)
-print s.get_data()
+if __name__ == "__main__":
+	mops = MOPS_fetch()
+	mops._test()
